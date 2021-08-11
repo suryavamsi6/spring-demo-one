fc96c92a44e711f08b773bf2c1cb5482506a962a
suryavamsi6 <d.suryavamsi@gmail.com>
2021-08-11 18:12:35 +0530

    feat(Dependency Injection): Add Dependency Injection by adding fortune service

---
+++ src/com/surya/springdemo/FortuneService.java
@@ -0,0 +1,6 @@
+package com.surya.springdemo;
+
+public interface FortuneService {
+
+    public String getFortune();
+}
--- src/com/surya/springdemo/HelloSpringApp.java
+++ src/com/surya/springdemo/HelloSpringApp.java
@@ -8,6 +8,7 @@ public class HelloSpringApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Coach theCoach = context.getBean("myCoach", Coach.class);
         System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
         context.close();
     }
 }
